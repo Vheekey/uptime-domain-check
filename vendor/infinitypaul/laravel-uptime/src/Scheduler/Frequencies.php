c69ba1d292bc6650b08538eaf7fb8ad1dbf015ee
Victoria Essien <>
2020-08-20 23:14:38 +0100

    updates via comment

---
+++ vendor/infinitypaul/laravel-uptime/src/Scheduler/Frequencies.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Infinitypaul\LaravelUptime\Scheduler;
+
+trait Frequencies
+{
+    protected $expression = '* * * * *';
+
+    public function everyMinutes($minutes = 1)
+    {
+        $this->expression = "*/{$minutes} * * * *";
+    }
+}
